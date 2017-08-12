9ef726f03c22fd379b143247e9c050ce13ee832b
Stacey Fenton <staceyfenton@gmail.com>
2017-08-12 21:43:35 +0100

    Add contact form

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,30 @@
-// Interpolate variable bindings
-var name = "Bob", time = "today";
-`Hello ${name}, how are you ${time}?`
+document.addEventListener('DOMContentLoaded', function() {
+	// contact form
+	let contactForm =  document.querySelector('.contact-form');
+	let emAdd = 'hello' + '@' + 'staceyfenton' + '.' + 'com';
+	contactForm.setAttribute('action', '//formspree.io/' + emAdd);
+	contactForm.addEventListener('submit', function(event) {
+		event.preventDefault();
+		let successMsg = '<div class="notification is-success">Thanks for contacting me, I will respond to you as soon as possible.</div>';
+		let errorMsg = '<div class="notification is-error">Oh no, something has gone wrong. Please try again.</div>';
+		let request = new Request('//formspree.io/' + emAdd, {
+			method: 'POST', 
+			body: new FormData(contactForm),
+			headers: new Headers({
+				'Accept': 'application/json'
+			})
+		});
+
+		fetch(request).then(function(response) {
+			console.log(response);
+			if(!response.ok) {
+				contactForm.insertAdjacentHTML('beforeend', errorMsg);
+				return Promise.reject(new Error(response.statusText));
+			}
+			contactForm.insertAdjacentHTML('beforeend', successMsg);
+			return Promise.resolve(response);
+		}).catch(function(error) {
+			contactForm.insertAdjacentHTML('beforeend', errorMsg);
+		});
+	});
+});
