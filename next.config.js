7b50b2c5684e30ec473ba2753431f298b7cce9af
Stacey Fenton <staceyfenton@gmail.com>
2020-09-14 17:45:24 +0100

    Add cdn url

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? '/staceyfenton.github.io/' : '',
+  assetPrefix: isProd ? 'https://d1nd6ebc4fb67p.cloudfront.net/' : '',
 }
