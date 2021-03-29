5deefdce8e9a19d439298fde74f3848d05f345d5
Stacey Fenton <staceyfenton@gmail.com>
2021-03-29 13:15:25 +0100

    deploy to s3 workflow

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-// const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production'
 
-// module.exports = {
-//   // Use the CDN in production and localhost for development.
-//   assetPrefix: isProd ? 'https://d1nd6ebc4fb67p.cloudfront.net/' : '',
-// }
+module.exports = {
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd ? 'https://d1nd6ebc4fb67p.cloudfront.net/' : '',
+}
