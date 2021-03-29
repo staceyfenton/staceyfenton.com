0463513e577ca7c4d4c225cab6760bf5eb90df17
Stacey Fenton <staceyfenton@gmail.com>
2021-03-29 14:43:37 +0100

    inject css into document head

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,27 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+class InlineStylesHead extends Head {
+  getCssLinks() {
+    return this.__getInlineStyles();
+  }
+
+  __getInlineStyles() {
+    const { assetPrefix, files } = this.context;
+    if (!files || files.length === 0) return null;
+
+    return files.filter(file => /\.css$/.test(file)).map(file => (
+      <style
+        key={file}
+        data-href={`${assetPrefix}/_next/${file}`}
+        dangerouslySetInnerHTML={{
+          __html: readFileSync(join(process.cwd(), '.build', file), 'utf-8'),
+        }}
+      />
+    ));
+  }
+}
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -9,7 +32,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" prefix="og: http://ogp.me/ns#">
-        <Head />
+        <InlineStylesHead />
         <body>
           <Main />
           <NextScript />
